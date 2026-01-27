fd6d5242540955aae6cec89b5e5b7dc69729e2bd
Theo <theo.stolzlechner3@gmail.com>
2026-01-27 14:15:58 +0100

    chore: clean rebuild start (remove old files)

--- .pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/firmware_msc_fat.c.o: \
- /home/main/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /home/main/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
